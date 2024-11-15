83fecb951d1ce6b1459625fb48d20aee9e0166d3
Vipul Verma <56539924+vipulverma2806@users.noreply.github.com>
2024-11-15 19:23:58 +0530

    Add files via upload

---
+++ qrcode.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("button");
+const qrimg = document.querySelector("img");
+const input = document.querySelector("input");
+button.addEventListener("click", () => {
+  if (input.value) {
+    qrimg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`;
+  }else{
+    alert('Provid valid URL')
+  }
+});
